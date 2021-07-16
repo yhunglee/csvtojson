0b416241f7ba68eb61cd3f9a68e498e6c0f16b32
Howard.Lee <howard@csie.io>
2021-07-17 07:59:42 +0800

    feat(app.module.ts): introduce CsvConversionComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CsvConversionComponent } from './components/csv-conversion/csv-conversion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CsvConversionComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [CsvConversionComponent]
 })
 export class AppModule { }
