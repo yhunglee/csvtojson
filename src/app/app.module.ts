932912d13d4effc5e7edff82f759e9ce5c4ea9e8
Howard.Lee <howard@csie.io>
2021-07-17 08:05:12 +0800

    feat(app.module.ts): introduce TableInfoComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CsvConversionComponent } from './components/csv-conversion/csv-conversion.component';
+import { TableInfoComponent } from './components/table-info/table-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CsvConversionComponent
+    CsvConversionComponent,
+    TableInfoComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [CsvConversionComponent]
+  exports: [CsvConversionComponent, TableInfoComponent]
 })
 export class AppModule { }
