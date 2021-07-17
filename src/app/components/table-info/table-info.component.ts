536079dd6472328bb1ec366e61114037fea2407e
Howard.Lee <howard@csie.io>
2021-07-17 10:03:42 +0800

    feat(table-info.component.ts): Prepare variables for rendering.

--- src/app/components/table-info/table-info.component.ts
+++ src/app/components/table-info/table-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table-info',
@@ -6,6 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-info.component.scss']
 })
 export class TableInfoComponent implements OnInit {
+
+  /**
+   * Use as table
+   */
+  @Input('tableAry')
+  get tableAry() {
+    return this._tableAry || [];
+  }
+  set tableAry(ary) {
+    this._tableAry = ary || [];
+  }
+
+  /**
+   * use as column name
+   */
+  get columnNameAry() {
+    return this.tableAry[0] || [];
+  }
+  set columnNameAry(ary) {
+    this._tableAry.splice(0, 1, ary);
+  }
+
+  /**
+   * Use as data row
+   */
+  get recordAry() {
+    let resultAry = [];
+    if (this.tableAry.length > 1) {
+      [, ...resultAry] = [...this.tableAry];
+    }
+
+    return resultAry;
+  }
+
+
+  private _tableAry: Array<Array<String>> = [];
+
+
 
   constructor() { }
 
