85ef114b2e9aba015be79e1817bcd5a461dcc84c
Howard.Lee <howard@csie.io>
2021-07-18 14:57:47 +0800

    feat(app.component): handle received csv data array and pass to table.
    handle received csv data array and then pass to table.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csvToJson';
+
+  /**
+   * Received data array from app-csv-conversion componenent.
+   *
+   */
+  receivedAry: Array<Array<string>> = [];
+
+  constructor() {
+
+  }
+
+  onReceiveCSVInfo(csvAry) {
+
+    this.receivedAry = csvAry;
+
+  }
 }
