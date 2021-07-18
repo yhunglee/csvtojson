57960b770a6b86426a1b0318c07a28d2f425a46b
Howard.Lee <howard@csie.io>
2021-07-18 23:09:36 +0800

    test(app.component.spec.ts): add a case for receiving csvAry from csvConversion

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,20 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { first } from 'rxjs/operators';
 import { AppComponent } from './app.component';
+import { CsvConversionComponent } from './components/csv-conversion/csv-conversion.component';
+import { TableInfoComponent } from './components/table-info/table-info.component';
 
 describe('AppComponent', () => {
+
+  let csvStr = '';
+  let expectedCsvAry: string[][] = [];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule],
       declarations: [
-        AppComponent
+        AppComponent,CsvConversionComponent,TableInfoComponent
       ],
     }).compileComponents();
   }));
@@ -22,10 +31,55 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('csvToJson');
   });
 
-  it('should render title', () => {
+  it(`should pass values from CsvConversionComponent to TableInfoComponent`, fakeAsync(() => {
+
+    // preparation for appComponent
     const fixture = TestBed.createComponent(AppComponent);
+    let appComponent = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('csvToJson app is running!');
-  });
+
+    // preparation for CsvConversionComponent
+    const fixtureCsv = TestBed.createComponent(CsvConversionComponent);
+    let csvComponent = fixtureCsv.componentInstance;
+    let csvAry: string[][] | undefined;
+    csvComponent.ngOnInit();
+
+    csvStr = `Name,Country,Company
+    Marcus,USA,Apple Inc.
+    Pei-Ming,Sweden,IKEA Furnitures
+    Yu-Han,Finland,Nokia Communications`;
+
+    expectedCsvAry = [
+      ['Name', 'Country', 'Company'],
+      ['Marcus', 'USA', 'Apple Inc.'],
+      ['Pei-Ming', 'Sweden', 'IKEA Furnitures'],
+      ['Yu-Han', 'Finland', 'Nokia Communications'],
+    ];
+
+    // set value to formCSV within csvComponent
+    fixtureCsv.detectChanges();
+    csvComponent.formCSV.setValue(csvStr);
+    fixtureCsv.detectChanges();
+
+
+    csvComponent.csvAry.pipe(first()).subscribe((val: any) => {
+      csvAry = val;
+    });
+
+    // wait for csv's then() applied
+    tick();
+
+    // examine csvComponent.csvAry is equal to expectedCsvAry
+    expect(csvAry).toEqual(expectedCsvAry);
+
+    // call appComponent.onReceiveCSVInfo
+    appComponent.onReceiveCSVInfo(csvAry);
+    // appComponent detectChanges()
+    fixture.detectChanges();
+
+    // examine appComponent.receivedAry
+    expect(appComponent.receivedAry).toEqual(expectedCsvAry);
+
+  }));
+
 });
